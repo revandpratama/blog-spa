6e62285fd8153c360c6c94ccd12395f0751d4088
Revand <revandaustin@gmail.com>
2023-07-01 12:15:27 +0700

    frscmmt

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Hello World</h1>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite';
+
+
+onMounted(() => {
+    initFlowbite
+})
+
+</script>
